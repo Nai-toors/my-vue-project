7e88fe3f912acc5e976397a6073587142dfc1bb6
Nai-toors <mesoulte@gmail.com>
2025-03-06 23:48:06 +0300

    chg: changed vite-port to 8082

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,12 +7,20 @@
     <div class="container">
       <div class="flex justify-content-between align-items-center">
         <div>
-          <p class="m-0 p-2">&copy; 2025 Vue Project. Все права защищены.</p>
+          <p class="m-0 p-1">&copy; 2025 Vue Project. Все права защищены.</p>
         </div>
         <div>
           <ul class="footer-links flex list-none m-0 p-0">
-            <li class="mr-3"><a href="#" class="text-white no-underline hover:text-blue-200">Политика конфиденциальности</a></li>
-            <li><a href="#" class="text-white no-underline hover:text-blue-200">Условия использования</a></li>
+            <li class="mr-3">
+              <a href="#" class="text-white no-underline hover:text-blue-200"
+                >Политика конфиденциальности</a
+              >
+            </li>
+            <li>
+              <a href="#" class="text-white no-underline hover:text-blue-200"
+                >Условия использования</a
+              >
+            </li>
           </ul>
         </div>
       </div>
